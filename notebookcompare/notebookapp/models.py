37f4c77c6b8a8f9035b9a600eea39f0cd4fe0e1d
Eduard <eduard@eduard-virtual-machine.(none)>
2013-04-03 11:55:28 +0200

    change models.py
    noves taules, country, component.

--- notebookcompare/notebookapp/models.py
+++ notebookcompare/notebookapp/models.py
@@ -2,18 +2,29 @@ from django.db import models
 from django.contrib.auth.models import User
 
 # Create your models here.
+class Country(models.Model):
+	name = models.TextField(max_length=30)
+	def __unicode__ (self):
+		return self.name
 
 class Brand(models.Model):
 	name = models.TextField(max_length=100)
-	country = models.TextField(max_length=100)
+	country = models.ForeignKey(Country)
 	web = models.TextField(max_length=100)
+	def __unicode__ (self):
+		return self.name
 
+class Component(models.Model):
+	name = models.TextField(max_length=30)
+	def __unicode__ (self):
+		return self.name
 
 class Specification (models.Model):
-	type_specs = models.TextField(max_length=100)
 	name = models.TextField(max_length=100)
+	component = models.ForeignKey(Component)
+	def __unicode__ (self):
+		return self.type_specs+" - " +self.name
 	
-
 class Model(models.Model):
 	name = models.TextField(max_length=100)
 	date = models.DateTimeField()
@@ -21,3 +32,5 @@ class Model(models.Model):
 	brand = models.ForeignKey(Brand)
 	specification = models.ManyToManyField(Specification)
 	useradd = models.ForeignKey(User)
+	def __unicode__ (self):
+		return self.name
