31c481432ea0681ff236512c46192dbe8b58be52
Eduard <root@eduard-virtual-machine.(none)>
2013-04-03 23:02:29 +0200

    NEW CSS! Now working on brands : List of brands, details of a brand.

--- notebookcompare/notebookapp/views.py
+++ notebookcompare/notebookapp/views.py
@@ -8,22 +8,26 @@ from django.shortcuts import render_to_response
 from django.http import Http404
 # 
 def mainpage(request):
-	return render_to_response('mainpage.html')
+	param = { 'titlehead' : "Home"}
+	return render_to_response('mainpage.html',param)
 
 #lists all the brands in the DB
 def brands(request):
-	all_brands = Brand.objects.all()
-	output = ', '.join([p.name + ' '+ p.web for p in all_brands])
+	param = { 'titlehead' : "List of all Brands"}
+	param['all_brands'] = Brand.objects.all()
+	#output = ', '.join([p.name + ' '+ p.web for p in all_brands])
 	#return HttpResponse(output)
-	return render_to_response('brands.html')
+	return render_to_response('brands.html',param)
+
+
 #list the details of a brand
 def brand_detail(request, brand_name):
 	try:
-		b = Brand.objects.get(name=brand_name)
+		brand= Brand.objects.get(name=brand_name)
+		param = {'titlehead' : "Brand Details",'name' : brand.name ,'web' : brand.web , 'country' : brand.country}
 	except Brand.DoesNotExist:
        	 raise Http404
-    	return render_to_response('detailbrand.html', {'brand': b},
-				context_instance=RequestContext(request))
+    	return render_to_response('detailbrand.html', param)
 
 
 
