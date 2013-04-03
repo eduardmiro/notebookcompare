c0037acd7109c9d258bb7ab2ba77cf1b4093f56b
root <root@eduard-virtual-machine.(none)>
2013-04-03 12:32:56 +0200

    admin change

--- notebookcompare/notebookapp/admin.py
+++ notebookcompare/notebookapp/admin.py
@@ -2,7 +2,12 @@ from django.contrib import admin
 from notebookapp.models import Brand
 from notebookapp.models import Model
 from notebookapp.models import Specification
+from notebookapp.models import Country
+from notebookapp.models import Component
+
 
 admin.site.register(Brand)
 admin.site.register(Model)
 admin.site.register(Specification)
+admin.site.register(Country)
+admin.site.register(Component)
