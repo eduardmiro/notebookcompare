cb9d39e98fb507de62a500f81eebd15049eae9f0
joma <joma@joma-V130.(none)>
2013-04-24 12:09:43 +0200

    Setting

--- notebookcompare/notebookcompare/settings.py
+++ notebookcompare/notebookcompare/settings.py
@@ -70,6 +70,8 @@ STATIC_URL = '/static/'
 # Additional locations of static files
 STATICFILES_DIRS = (
 	"/home/eduard/sisweb/notebookcompare/notebookcompare/static",
+	"/home/joma/SistemesWeb/Practica1/notebookcompare/notebookcompare/static",
+
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
